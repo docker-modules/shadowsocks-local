e8f2ef5c268f544a6804034d6480eb68966d1a12
Vincent <const.lomo@gmail.com>
2018-11-16 16:19:50 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 nohup ss-local -s $SERVER -p $PORT -m $METHOD -k $PASSWORD -t $TIMEOUT -b 0.0.0.0 -l 1081 -u --fast-open &
 nohup polipo proxyAddress=0.0.0.0 proxyPort=1080 socksProxyType=socks5 socksParentProxy=127.0.0.1:1081 &
 
-exec "tail -f /dev/null"
+exec tail -f /dev/null
